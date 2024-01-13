a2b2df804f5fe6419b98fb02bc19e710458832d3
ANIMAZE0 <153982983+ANIMAZE0@users.noreply.github.com>
2024-01-12 23:27:21 -0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import time
 
 class Config(object):
 
-    TELEGRAM_BOT_TOKEN    = os.environ.get("TELEGRAM_BOT_TOKEN", "")
+    TELEGRAM_BOT_TOKEN    = os.environ.get("TELEGRAM_BOT_TOKEN", "6851266521:AAHY5uq7jUQQPHSR5ad-vMoykyFVSeDR4sg")
     API_KEY  = os.environ.get("API_KEY", "nNVB1ecq09EhrAujk99r")
     GET_API_KEY  = os.environ.get("GET_API_KEY", "https://anbusec.xyz")
 
